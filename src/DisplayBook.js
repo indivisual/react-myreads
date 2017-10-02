5dbd00852401df701bebfbca81b36a876eca88f7
Carlos Prat <carlosprat@gmail.com>
2017-10-02 13:01:22 +0200

    Udacity Project

--- src/DisplayBook.js
+++ src/DisplayBook.js
@@ -8,6 +8,7 @@ class DisplayBook extends Component {
     render() {
 
         const { book, onMoving } = this.props
+        let section = book.shelf !== undefined ? book.shelf : 'none';
 
         return (
             <div className="book">
@@ -16,12 +17,14 @@ class DisplayBook extends Component {
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
                
                 <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={(event) => onMoving(book, event.target.value)}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    
+                    <select value={section} onChange={(event) => onMoving(book, event.target.value)}>
+                        
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
                     </select>
                 </div>
                 </div>
